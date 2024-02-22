d959a6c1136fd453e217e465bc9d1b4fd62066eb
Hinkala <prmt256790@gmail.com>
2024-02-22 18:12:40 +0300

    The newest version

--- app/controller.py
+++ app/controller.py
@@ -1,11 +1,15 @@
 from app import app
-from flask import render_template
-
+from flask import render_template, url_for
 
 @app.route('/')
+@app.route('/index')
 def index_get():
-    return render_template('index.html',a1 = 'Hello',g1 = 'Hello')
+    return render_template('index.html')
 
 @app.route('/one')
 def one_get():
     return render_template('one.html')
+
+@app.route('/two')
+def two_get():
+    return render_template('two.html')
