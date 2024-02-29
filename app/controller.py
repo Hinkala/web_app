e2d11281c47081aeebdbe639c88870b1124eebb1
Hinkala <prmt256790@gmail.com>
2024-02-29 18:33:00 +0300

    sdasdsds

--- app/controller.py
+++ app/controller.py
@@ -1,10 +1,15 @@
-from app import app
+from app import app,model
 from flask import render_template, url_for
 
 @app.route('/')
 @app.route('/index')
 def index_get():
-    return render_template('index.html')
+    cats = []
+    cats.append(model.Cat(1, 'Naf', 'Red', 2, 'cat1.png'))
+    cats.append(model.Cat(2, 'Bars', 'White', 6, 'cat2.png'))
+    cats.append(model.Cat(3, 'Simba', 'Gold', 4, 'cat3.png'))
+    cats.append(model.Cat(4, 'Mars', 'Black', 3, 'cat4.png'))
+    return render_template('index.html',cat_list = cats)
 
 @app.route('/one')
 def one_get():
